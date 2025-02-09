e13e324fb9aa8089194c90e1b9d8d1c443368830
Nick Lipple <915617+nicklipple@users.noreply.github.com>
2025-02-09 18:57:52 +0000

    templ

--- init.lua
+++ init.lua
@@ -609,6 +609,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        templ = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
